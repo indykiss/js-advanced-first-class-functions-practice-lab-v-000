79be5dfe4a5e106090d6da0a8e7e5a0f2d4c98c0
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-07 16:48:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function driversByName(drivers) {
   let newDriver = [...drivers]
     
   let sortedDrivers = newDriver.sort(function(a, z) {
-    return a.localeCompare(z)
+    return a.localeCompare(z.names)
   })
   return sortedDrivers
 
